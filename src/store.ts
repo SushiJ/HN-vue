a4318b7c8da2397dacd064d78106ec9ac6d73049
Sushij <sushant.wurk@gmail.com>
2023-04-26 22:16:11 +0530

    Added FetchNewStories to store

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,22 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-type TopStories = {
+export type TopStories = {
   by: string;
-  decendants: number;
+  descendants: number;
   id: number;
   kids: Array<number>;
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+};
+
+export type NewStories = {
+  by: string;
+  descendants: number;
+  id: number;
   score: number;
   time: number;
   title: string;
@@ -16,16 +27,19 @@ type TopStories = {
 export const useStore = defineStore("stories", {
   state: () => ({
     topStories: [] as TopStories[],
-    newStories: [],
+    newStories: [] as NewStories[],
   }),
   actions: {
-    fetchTopStories() {
+    fetchTopStories(pageNumber: number) {
       axios
         .get<Array<string>>(
           "https://hacker-news.firebaseio.com/v0/topstories.json"
         )
         .then((resp) => {
-          const results = resp.data.slice(0, 30);
+          const results = resp.data.slice(
+            pageNumber * 30,
+            pageNumber === 0 ? 30 : pageNumber * 30 * 2
+          );
           results.forEach((element: string) => {
             axios
               .get(`https://hacker-news.firebaseio.com/v0/item/${element}.json`)
@@ -38,6 +52,27 @@ export const useStore = defineStore("stories", {
           });
         })
         .catch((err) => {
+          console.log(err);
+        });
+    },
+    fetchNewStories() {
+      axios
+        .get("https://hacker-news.firebaseio.com/v0/newstories.json")
+        .then((resp) => {
+          const results = resp.data.slice(0, 25);
+          results.forEach((element: string) => {
+            axios
+              .get(`https://hacker-news.firebaseio.com/v0/item/${element}.json`)
+              .then((result) => {
+                this.newStories.push(result.data);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+        })
+        .catch((err) => {
+          /* eslint-disable */
           console.log(err);
         });
     },
