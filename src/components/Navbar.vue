b50db1c7446c84ccaa7500ac9680fe5825137392
Sushij <sushant.wurk@gmail.com>
2023-05-04 22:35:44 +0530

    Added Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="pure-menu">
+    <div class="container">
+      <router-link :to="{ path: '/' }">Home</router-link>
+      <router-link :to="{ path: '/new' }">New</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pure-menu {
+  background-color: #e17842;
+  margin-bottom: 0.5rem;
+}
+.pure-menu a {
+  color: white;
+  padding-right: 1rem;
+}
+</style>
